d7587fd346005c7fb37a540e701d9497e807ad12
Emerson Cedeno <emerson.cedeno@logicstudio.net>
2019-04-18 16:41:55 -0500

    Archivos mínimo necesarios para la creación de un componente en Joomla

---
+++ administrator/components/com_todolist/todolist.php
@@ -0,0 +1,6 @@
+<?php 
+/**
+ * To-do list administrator entry point
+ */
+
+echo 'Hello World.';
---
+++ components/com_todolist/todolist.php
@@ -0,0 +1,6 @@
+<?php 
+/**
+ * To-do list frontend entry point
+ */
+
+echo 'This file is intentionally blank.';
